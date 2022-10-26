a157ce8a14368b23eee873ca5ba94ce83b4e1abf
Arda Özkardeşler <aozkardesler@gmail.com>
2022-10-26 17:51:52 +0300

    Add methods for storing notification preferences
    *Convert UI element for Languages from dropdown to toggle group

--- Assets/SettingsAPI/SettingsAPI.cs
+++ Assets/SettingsAPI/SettingsAPI.cs
@@ -4,38 +4,70 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class SettingsAPI : MonoBehaviour
 {
     public TMP_InputField nicknameInputField;
-    public TMP_Dropdown languageDropdown;
     public TMP_Text greetingMessage;
-    private Sprite avatarSprite;
     public Button selectAvatarButton;
+    public Toggle dailyReminderToggle;
+    public Toggle weeklyReminderToggle;
+    public Toggle usabilityTipsToggle;
+    public Toggle promotionsNotificationToggle;
+    public ToggleGroup languages;
     AssistiveCardsSDK assistiveCardsSDK;
     public GameObject SDK;
 
     private string nickname;
     private string language;
+    private string reminderPreference;
+    private bool isUsabilityTipsActive;
+    private bool isPromotionsNotificationActive;
+
 
     private async void Awake()
     {
         assistiveCardsSDK = SDK.GetComponent<AssistiveCardsSDK>();
         nickname = GetNickname();
         language = GetLanguage();
+        isUsabilityTipsActive = GetUsabilityTipsPreference() == 1 ? true : false;
+        isPromotionsNotificationActive = GetPromotionsNotificationPreference() == 1 ? true : false;
         nicknameInputField.text = nickname;
         selectAvatarButton.image.sprite = await GetAvatarImage();
     }
     private void Start()
     {
-        greetingMessage.text = "Hello, " + nickname + " you have selected the language: " + language;
+        reminderPreference = GetReminderPreference();
+        usabilityTipsToggle.isOn = GetUsabilityTipsPreference() == 1 ? true : false;
+        promotionsNotificationToggle.isOn = GetPromotionsNotificationPreference() == 1 ? true : false;
+        foreach (var toggle in languages.GetComponentsInChildren<Toggle>())
+        {
+            if (toggle.name == language)
+            {
+                toggle.isOn = true;
+            }
+        }
+
+        greetingMessage.text = "Hello " + nickname + ", you have selected the language " + language + ". Your reminder period preference is " + reminderPreference + ". You " + (isUsabilityTipsActive ? "will" : "won't") + " receive usability tips. You " + (isPromotionsNotificationActive ? "will" : "won't") + " receive promotion notifications.";
+        if (reminderPreference == "Daily")
+        {
+            dailyReminderToggle.isOn = true;
+        }
+        else
+        {
+            weeklyReminderToggle.isOn = true;
+        }
 
     }
     private void Update()
     {
         nickname = GetNickname();
         language = GetLanguage();
-        greetingMessage.text = "Hello, " + nickname + " you have selected the language: " + language;
+        reminderPreference = GetReminderPreference();
+        isUsabilityTipsActive = GetUsabilityTipsPreference() == 1 ? true : false;
+        isPromotionsNotificationActive = GetPromotionsNotificationPreference() == 1 ? true : false;
+        greetingMessage.text = "Hello " + nickname + ", you have selected the language " + language + ". Your reminder period preference is " + reminderPreference + ". You " + (isUsabilityTipsActive ? "will" : "won't") + " receive usability tips. You " + (isPromotionsNotificationActive ? "will" : "won't") + " receive promotion notifications.";
     }
 
     public void SetNickname(string nickname)
@@ -71,10 +103,42 @@ public class SettingsAPI : MonoBehaviour
         return sprite;
     }
 
+    public void SetReminderPreference(string period)
+    {
+        PlayerPrefs.SetString("ReminderPeriod", period);
+    }
+
+    public string GetReminderPreference()
+    {
+        return PlayerPrefs.GetString("ReminderPeriod", "Weekly");
+    }
+
+    public void SetUsabilityTipsPreference(int isUsabilityTipsActive)
+    {
+        PlayerPrefs.SetInt("UsabilityTipsPreference", isUsabilityTipsActive);
+    }
+
+    public int GetUsabilityTipsPreference()
+    {
+        return PlayerPrefs.GetInt("UsabilityTipsPreference", 0);
+    }
+
+    public void SetPromotionsNotificationPreference(int isPromotionsNotificationActive)
+    {
+        PlayerPrefs.SetInt("PromotionsNotificationPreference", isPromotionsNotificationActive);
+    }
+
+    public int GetPromotionsNotificationPreference()
+    {
+        return PlayerPrefs.GetInt("PromotionsNotificationPreference", 0);
+    }
 
     public void SaveSettings()
     {
         SetNickname(nicknameInputField.text);
-        SetLanguage(languageDropdown.options[languageDropdown.value].text);
+        SetLanguage(languages.ActiveToggles().FirstOrDefault().GetComponentInChildren<Text>().text);
+        SetReminderPreference(dailyReminderToggle.isOn ? "Daily" : "Weekly");
+        SetUsabilityTipsPreference(usabilityTipsToggle.isOn ? 1 : 0);
+        SetPromotionsNotificationPreference(promotionsNotificationToggle.isOn ? 1 : 0);
     }
 }
